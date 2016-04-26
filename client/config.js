1788dd74928c26c8cd50be77680b96aa09e94c73
cb <chase@Chase.central.yale.internal>
2016-04-26 15:27:00 -0400

    Updating config
    Adding [core] and [remote] to config.js for Heroku

--- client/config.js
+++ client/config.js
@@ -3,3 +3,11 @@ $.cloudinary.config({
     api_key: '243461195879357'
 });
 
+[core]
+        repositoryformatversion = 0
+        filemode = true
+        bare = false
+        logallrefupdates = true
+[remote "heroku"]
+        url = git@heroku.com:compostheap.git
+        fetch = +refs/heads/*:refs/remotes/heroku/*
