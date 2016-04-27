62e4ab8f412ef9ab5f3ee0ab3f34bbfacb98faf6
cb <chase@Chase.central.yale.internal>
2016-04-27 18:37:32 -0400

    attempt to fix unknown cloud_name

--- server/config.js
+++ server/config.js
@@ -4,3 +4,8 @@ Cloudinary.config({
     api_secret: 'cWBZYSkOMoDPtdFHp47ddarA4P4'
 });
 
+$.cloudinary.config({
+    cloud_name: 'dnyeyqkyk',
+    api_key: '243461195879357',
+    api_secret: 'cWBZYSkOMoDPtdFHp47ddarA4P4'
+});
